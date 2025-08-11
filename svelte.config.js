322640d94ebd44de1cb6069d022087e3b3e48f98
FAIT ltd <126212038+Faitltd@users.noreply.github.com>
2025-08-11 06:04:57 -0600

    build: use vitePreprocess from @sveltejs/vite-plugin-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
     files: {
-      assets: 'static',
-      lib: 'src/lib',
-      routes: 'src/routes',
       appTemplate: 'src/app.html'
     }
   }
